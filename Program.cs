c1044fb2206f07c2b9d008b4257516b4d3d9f456
Serg <axenowsw@yandex.ru>
2021-12-01 00:22:40 +0300

    My Solution first task

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int FindDuplicate(List<string> name, List<int> price, List<int> weight)
+{
+
+	if (name.Count == price.Count && price.Count == weight.Count)
+	{
+		List<object[]> products = new List<object[]>();
+		for (int i = 0; i < name.Count; i++)
+		{
+			products.Add(new object[] { name[i], price[i], weight[i] });
+		}
+		var count = products
+			.GroupBy(x => new { Name = x[0], Price = x[1], Weight = x[2] })
+			.Select(x => new { Prod = x, Count = x.Count() - 1 })
+			.Where(x => x.Count > 0)
+			.Sum(x => x.Count);
+		return count;
+	}
+	throw new Exception("Different length of lists");
+}
